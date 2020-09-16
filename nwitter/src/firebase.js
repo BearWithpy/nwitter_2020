681c33a1defeb2a98fbc69190c1044c179bd9c73
박준수 <pksjmh5295@gmail.com>
2020-09-16 22:09:30 +0900

    1.1 Setup

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAczCs70jeZzFvykL0Ma6WMMAyZHGRxgVI",
+  authDomain: "nwitter-bb675.firebaseapp.com",
+  databaseURL: "https://nwitter-bb675.firebaseio.com",
+  projectId: "nwitter-bb675",
+  storageBucket: "nwitter-bb675.appspot.com",
+  messagingSenderId: "1069558506829",
+  appId: "1:1069558506829:web:d5ff95b6792372e9530304",
+};
+
+export default firebase.initializeApp(firebaseConfig);
