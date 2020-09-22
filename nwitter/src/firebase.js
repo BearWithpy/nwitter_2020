0c023ef11a94fffc4aae9886e23a0c2ab3fcc634
박준수 <pksjmh5295@gmail.com>
2020-09-22 16:08:49 +0900

    2.0 Routers

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,13 +1,16 @@
 import * as firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAczCs70jeZzFvykL0Ma6WMMAyZHGRxgVI",
-  authDomain: "nwitter-bb675.firebaseapp.com",
-  databaseURL: "https://nwitter-bb675.firebaseio.com",
-  projectId: "nwitter-bb675",
-  storageBucket: "nwitter-bb675.appspot.com",
-  messagingSenderId: "1069558506829",
-  appId: "1:1069558506829:web:d5ff95b6792372e9530304",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
--- nwitter/src/index.js
+++ nwitter/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import firebase from "./firebase";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,5 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-//20200921
